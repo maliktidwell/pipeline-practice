46b5139957047a0403ed8645b3ed8dee4f76e158
Malik Tidwell <maliktidwell@Maliks-MacBook-Pro.local>
2026-08-12 22:52:11 -0400

    added priv subnet with ngw

--- variables.tf
+++ variables.tf
@@ -15,3 +15,9 @@ variable "public_subnet_cidr" {
   type        = string
   default     = "10.0.1.0/24"
 }
+
+variable "private_subnet_cidr" {
+  description = "The CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.11.0/24"
+}
--- vpc.tf
+++ vpc.tf
@@ -14,6 +14,7 @@ resource "aws_vpc" "vpc" {
   enable_dns_hostnames = true
 }
 
+#Public Subnet
 resource "aws_subnet" "public_a" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = var.public_subnet_cidr
@@ -37,4 +38,35 @@ resource "aws_route_table" "public_rt" {
 resource "aws_route_table_association" "public_a_assoc" {
   subnet_id      = aws_subnet.public_a.id
   route_table_id = aws_route_table.public_rt.id
+}
+
+#Private Subnet
+resource "aws_subnet" "private_a" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.private_subnet_cidr
+  availability_zone = local.az1
+}
+
+resource "aws_eip" "eip" {
+  domain   = "vpc"
+}
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public_a.id
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+}
+
+resource "aws_route_table_association" "private_a_assoc" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_rt.id
 }
